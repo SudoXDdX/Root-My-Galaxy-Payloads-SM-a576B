89a262c2b88f1b5bf54b27d4f376c4c8c6f8b936
SudoXDdX <theopagliarinifariaa@gmail.com>
2026-08-12 15:47:15 -0300

    fix jhash2: remove (length<<2) from initval (kernel 6.12 Samsung)

--- src/kernelsnitch/futex_hash.h
+++ src/kernelsnitch/futex_hash.h
@@ -103,7 +103,7 @@ static inline u32 jhash2(const u32 *k, u32 length, u32 initval)
     u32 a, b, c;
 
     /* Set up the internal state */
-    a = b = c = JHASH_INITVAL + (length<<2) + initval;
+    a = b = c = JHASH_INITVAL + initval;
 
     /* Handle most of the key */
     while (length > 3) {
@@ -186,9 +186,10 @@ typedef union {
 
 uint32_t futex_hash_no_trunc(futex_key_t *key)
 {
+    
+    
     uint32_t hash = jhash2((uint32_t *)key, OFFSET_OF(typeof(*key), both.offset) / 4,
               key->both.offset);
-
     return hash;
 }
 
